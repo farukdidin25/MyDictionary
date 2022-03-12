7e906ad718393f76baf93941460c112ebb1ffce8
Ömer Faruk DİDİN <fdidin59@gmail.com>
2022-03-12 11:17:16 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+
+Ogrenci.Add(157, "Burcu Taş");
+Ogrenci.Add(356, "Halil Kaya");
+Ogrenci.Add(291, "Pınar Durak");
+Ogrenci.Add(680, "Emel Akasya");
+Ogrenci.Add(423, "Yusuf Deniz");
+
+
+Ogrenci.Yazdir();
+
+
